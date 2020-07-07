065ec3cced1107925b4eb955063a673482239e9e
Jason Tremblay <alertmybanjos@users.noreply.github.com>
2020-07-07 17:50:01 -0400

    Better handling of scroll-triggered playback in video module

--- modules/ambbb-video/src/autoplay-scroll.js
+++ modules/ambbb-video/src/autoplay-scroll.js
@@ -7,52 +7,68 @@ video_els.forEach( video_el => {
 
   var video_player = videojs( video_el.dataset.video_id );
 
-  // … scrolling down, video fully enters view —> play
-  var video_enters_bottom = new Waypoint( {
-    element: video_el,
-    handler: function( direction ) {
-      if ( 'down' === direction ) {
-        video_player.play();
-      }
-    },
-    offset: 'bottom-in-view'
-  } );
+  video_player.ready( function() {
 
-  // … scrolling down, video fully leaves view —> pause and rewind
-  var video_exits_top = new Waypoint( {
-    element: video_el,
-    handler: function( direction ) {
-      if ( 'down' === direction ) {
-        video_player.pause();
-        video_player.currentTime( 0 );
-      }
-    },
-    offset: function() {
-      return -1 * video_player.currentHeight()
-    }
-  } );
+    // … scrolling down, video fully enters view —> play
+    var video_enters_bottom = new Waypoint( {
+      element: video_el,
+      handler: function( direction ) {
+        if (
+          'down' === direction
+          && video_player.paused()
+        ) {
+          video_player.play();
+        }
+      },
+      offset: 'bottom-in-view'
+    } );
 
-  // … scrolling up, video fully enters view —> play
-  var video_enters_top = new Waypoint( {
-    element: video_el,
-    handler: function( direction ) {
-      if ( 'up' === direction ) {
-        video_player.play();
+    // … scrolling down, video fully leaves view —> pause and rewind
+    var video_exits_top = new Waypoint( {
+      element: video_el,
+      handler: function( direction ) {
+        if (
+          'down' === direction
+          && !video_player.paused()
+        ) {
+          video_player.pause();
+          video_player.currentTime( 0 );
+        }
+      },
+      offset: function() {
+        return -1 * video_player.currentHeight()
       }
-    },
-    offset: 0
-  } );
+    } );
 
-  // … scrolling up, video fully leaves view —> pause and rewind
-  var video_exits_bottom = new Waypoint( {
-    element: video_el,
-    handler: function( direction ) {
-      if ( 'up' === direction ) {
-        video_player.pause();
-        video_player.currentTime( 0 );
-      }
-    },
-    offset: '100%'
+    // … scrolling up, video fully enters view —> play
+    var video_enters_top = new Waypoint( {
+      element: video_el,
+      handler: function( direction ) {
+        if (
+          'up' === direction
+          && video_player.paused()
+        ) {
+          video_player.play();
+        }
+      },
+      offset: 0
+    } );
+
+    // … scrolling up, video fully leaves view —> pause and rewind
+    var video_exits_bottom = new Waypoint( {
+      element: video_el,
+      handler: function( direction ) {
+        if (
+          'up' === direction
+          && !video_player.paused()
+        ) {
+          video_player.pause();
+          video_player.currentTime( 0 );
+        }
+      },
+      offset: '100%'
+    } );
+
   } );
 
 } );
