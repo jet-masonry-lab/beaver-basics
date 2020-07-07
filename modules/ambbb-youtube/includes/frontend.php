d9a5acd088f792021a297f9e5c8e588c3ae02567
Jason Tremblay <alertmybanjos@users.noreply.github.com>
2020-07-07 16:19:03 -0400

    Play YouTube embeds inline on ios

--- modules/ambbb-youtube/includes/frontend.php
+++ modules/ambbb-youtube/includes/frontend.php
@@ -10,7 +10,7 @@
   <?php if ( $module->has( 'youtube_id' ) ) : ?>
     <iframe
       class="<?= esc_attr( $module->classes( 'iframe' ) ); ?>"
-      src='https://www.youtube.com/embed/<?= $settings->youtube_id ?>?rel=0&amp;showinfo=0'
+      src='https://www.youtube.com/embed/<?= $settings->youtube_id ?>?rel=0&amp;showinfo=0&amp;playsinline=1'
       frameborder='0'
       allowfullscreen
     ></iframe>
