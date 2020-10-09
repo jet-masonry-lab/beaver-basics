719675f06ac4eac05a46bb4b979a2dc6abba62be
Jason Tremblay <alertmybanjos@users.noreply.github.com>
2020-10-08 21:24:58 -0400

    Remove stray anchor tag

--- modules/ambbb-button/includes/frontend.php
+++ modules/ambbb-button/includes/frontend.php
@@ -5,8 +5,6 @@
 // - $settings
 ?>
 
-<a >
-
 <a class="<?= esc_attr( $module->classes() ); ?>" href="<?= esc_url( $settings->link ); ?>" <?= $module->linkAttrs( 'link' ); ?> role="button">
   <span class="<?= esc_attr( $module->classes( 'text' ) ); ?>"><?= $module->escInlineHtml( $settings->text ); ?></span>
 </a>
