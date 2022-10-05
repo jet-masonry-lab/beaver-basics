65f6ad8ad918bba30f2ba0fb542d4880431beea7
Jason Tremblay <4190+alertmybanjos@users.noreply.github.com>
2022-10-04 21:41:08 -0400

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import postcss from 'rollup-plugin-postcss';
